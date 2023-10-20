1aadaaa46aba3505f3e837f080e1bda69529048d
davidkupyn <davidkupyn@gmail.com>
2023-10-20 13:57:19 +0200

    Add go-learn code

--- main.go
+++ main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/sjwhitworth/golearn/base"
+	"github.com/sjwhitworth/golearn/evaluation"
+	"github.com/sjwhitworth/golearn/knn"
+)
+
+func main2() {
+	start := time.Now()
+
+	rawData, err := base.ParseCSVToInstances("iris.csv", true)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(rawData)
+
+	cls := knn.NewKnnClassifier("euclidean", "linear", 2)
+
+	trainData, testData := base.InstancesTrainTestSplit(rawData, 0.50)
+	cls.Fit(trainData)
+
+	predictions, err := cls.Predict(testData)
+	if err != nil {
+		panic(err)
+	}
+
+	confusionMat, err := evaluation.GetConfusionMatrix(testData, predictions)
+	if err != nil {
+		panic(fmt.Sprintf("Unable to get confusion matrix: %s", err.Error()))
+	}
+	fmt.Println(evaluation.GetSummary(confusionMat))
+
+	fmt.Println("Time elapsed:", time.Since(start))
+}
---
+++ neural-network.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/go-gota/gota/dataframe"
+)
+
+type IrisClassifer struct {
+}
+
+func (c *IrisClassifer) Fit(X, y interface{}) {
+	
+}
+
+func main() {
+	datasetFile, err := os.Open("iris.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer datasetFile.Close()
+
+	dataset := dataframe.ReadCSV(datasetFile)
+
+	fmt.Println(dataset)
+}
